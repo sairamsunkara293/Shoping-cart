2cb6552a4b801318440355bd5ef00efb26e412ce
sairamsunkara293 <sairam.sunkara1995@gmail.com>
2020-07-12 23:43:06 +0530

    modified app module

--- Shoping-cart/src/app/app.module.ts
+++ Shoping-cart/src/app/app.module.ts
@@ -4,31 +4,25 @@ import { AppRoutingModule, AppRoutes } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
-// import { RecipesComponent } from './recipes/recipes.component';
+
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-// import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-// import { ShopingListComponent } from './shoping-list/shoping-list.component';
+
 import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShopingListService } from './shoping-list/shoping-list.service';
-// import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-// import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    // RecipesComponent,
     RecipeListComponent,
-    // RecipeDetailComponent,
     RecipeItemComponent,
-    // ShopingListComponent,
     ShopingEditComponent,
     AppRoutes,
     DropdownDirective,
-    // RecipeEditComponent,
-    // RecipeStartComponent
   ],
   imports: [
     BrowserModule,
